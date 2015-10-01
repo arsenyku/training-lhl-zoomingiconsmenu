eb928f55b68155573d32180e317ab6c4a8f0e968
asu <arsenyk.ustaris.dev@gmail.com>
2015-10-01 11:58:07 -0700

    Adds icon assets.  Displays five cells in collection view.

--- ZoomingIconsMenu/MenuViewController.swift
+++ ZoomingIconsMenu/MenuViewController.swift
@@ -9,5 +9,29 @@
 import UIKit
 
 class MenuViewController: UICollectionViewController{
+
+    // MARK: - UICollectionViewController
     
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 2
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+
+        switch (section){
+        case 0:
+            return 2
+        case 1:
+            return 3
+        default:
+            return 0
+        }
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("zoomingIconCell", forIndexPath: indexPath)
+        
+
+        return cell
+    }
 }

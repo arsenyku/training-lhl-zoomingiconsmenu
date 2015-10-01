0596cdd4662705ec353380a7f01883af852f85a5
asu <arsenyk.ustaris.dev@gmail.com>
2015-10-01 11:04:27 -0700

    Initial Commit

---
+++ ZoomingIconsMenuTests/ZoomingIconsMenuTests.swift
@@ -0,0 +1,36 @@
+//
+//  ZoomingIconsMenuTests.swift
+//  ZoomingIconsMenuTests
+//
+//  Created by asu on 2015-10-01.
+//  Copyright © 2015 asu. All rights reserved.
+//
+
+import XCTest
+@testable import ZoomingIconsMenu
+
+class ZoomingIconsMenuTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

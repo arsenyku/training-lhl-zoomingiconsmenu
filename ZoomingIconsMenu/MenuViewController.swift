6c6f2ebbee9c5225e98261599275e5e4cf5c7845
asu <arsenyk.ustaris.dev@gmail.com>
2015-10-01 11:36:56 -0700

    Basic UI setup with custom view controller class.

---
+++ ZoomingIconsMenu/MenuViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MenuViewController.swift
+//  ZoomingIconsMenu
+//
+//  Created by asu on 2015-10-01.
+//  Copyright © 2015 asu. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UICollectionViewController{
+    
+}
